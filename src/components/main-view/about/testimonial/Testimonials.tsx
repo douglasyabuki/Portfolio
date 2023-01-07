55fc79795653496b514a2942588bf411108a0b76
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-07 10:10:43 -0300

    Commit 11: Add testimonials component

---
+++ src/components/main-view/about/testimonial/Testimonials.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface ITestimonialsProps {
+}
+
+export default function Testimonials (props: ITestimonialsProps) {
+  return (
+    <div>
+
+    </div>
+  );
+}
---
+++ src/interfaces/ITestimonial.ts
@@ -0,0 +1,7 @@
+// Interface of items on testimonial-list.ts
+export interface ITestimonial {
+  id: number;
+  position: string;
+  relation: string;
+  testimonial: string;
+}
