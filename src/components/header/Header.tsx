500969e3bc89702e15741ac38d24182238c2273f
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-07 08:52:09 -0300

    Commit 8: Responsive header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,21 @@
+// Const
+import { navigationList } from './navigation/navigation-list/navigation-list';
+import { socialList } from './social/social-list/social-list';
+
+// Components
+import Logo from './logo/Logo';
+import Navigation from './navigation/Navigation';
+import Social from './social/Social';
+
 // Header main function
-export default function Header () {
+export default function Header() {
 
   // Returns the whole header to App.tsx
   return (
-    <div>
-
-    </div>
+    <header className="group container justify-between fixed top-0 z-30 flex h-11 w-screen min-w-full bg-gradient-to-b from-background-black to-transparent px-5 py-1 text-center duration-500 hover:bg-background-black md:h-16 md:px-8 md:py-2 lg:px-12 text-not-so-white">
+      <Logo></Logo>
+      <Social list={socialList}></Social>
+      <Navigation list={navigationList}></Navigation>
+    </header>
   );
 }
