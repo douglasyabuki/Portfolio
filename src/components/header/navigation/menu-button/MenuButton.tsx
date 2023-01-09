83fff859e353b64e9e97beb38bb2ab1e3202e814
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-09 08:27:44 -0300

    Commit 26: Minor changes

--- src/components/header/navigation/menu-button/MenuButton.tsx
+++ src/components/header/navigation/menu-button/MenuButton.tsx
@@ -8,7 +8,7 @@ export default function MenuButton ({onClickHandler}: Props) {
 
   // Returns the button menu to display the navigation list on Navigation.tsx
   return (
-    <div  className="block container h-auto w-14 opacity-50 text-end hover:opacity-80 transition-opacity duration-150 lg:hidden">
+    <div  className="block container h-auto w-14 md:w-[75px] opacity-50 text-end hover:opacity-80 transition-opacity duration-150 lg:hidden">
       <i className="fa fa-bars text-xl md:text-3xl" role={'button'} onClick={onClickHandler}></i>
     </div>
   );
