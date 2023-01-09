7cd4aee7cacfc78bf046fe77829e9f262b237d5b
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-09 07:53:46 -0300

    Commit 24: A simple formatting change

--- src/components/main-view/projects/Projects.tsx
+++ src/components/main-view/projects/Projects.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 
-export interface IProjectsProps {
-}
+export interface IProjectsProps {}
 
-export default function Projects (props: IProjectsProps) {
+export default function Projects(props: IProjectsProps) {
   return (
-    <div id='projects' className="w-auto h-auto py-16 px-4 sm:px-7 md:px-8 lg:px-10 bg-background-div2">
-      <div className="px-4 sm:px-7 md:px-8 lg:px-10 rounded-xl bg-red-700">
-        Project works
-      </div>
+    <div
+      id="projects"
+      className="h-auto w-auto bg-background-div2 py-16 px-4 sm:px-7 md:px-8 lg:px-10"
+    >
+      <div className="rounded-xl bg-red-700 px-4 sm:px-7 md:px-8 lg:px-10">Project works</div>
     </div>
   );
 }
