aa5c003a8a23eb5b85c9cec404a2a90bf7148dac
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-09 08:22:24 -0300

    Commit 25: Fixed social links not being centered

--- src/components/header/navigation/menu-button/MenuButton.tsx
+++ src/components/header/navigation/menu-button/MenuButton.tsx
@@ -8,8 +8,8 @@ export default function MenuButton ({onClickHandler}: Props) {
 
   // Returns the button menu to display the navigation list on Navigation.tsx
   return (
-    <div role={'button'} onClick={onClickHandler} className="block container h-auto w-9 opacity-50 hover:opacity-80 transition-opacity duration-150 lg:hidden">
-      <i className="fa fa-bars text-xl md:text-3xl"></i>
+    <div  className="block container h-auto w-14 opacity-50 text-end hover:opacity-80 transition-opacity duration-150 lg:hidden">
+      <i className="fa fa-bars text-xl md:text-3xl" role={'button'} onClick={onClickHandler}></i>
     </div>
   );
 }
--- src/components/header/social/Social.tsx
+++ src/components/header/social/Social.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 export default function Social({ list }: Props) {
   return (
-    <div className="container w-min bg-black bg-opacity-0 gap-3 px-5 rounded-2xl flex self-center group-hover:bg-opacity-80 transition-bg-opacity ease-in-out duration-300 lg:hidden">
+    <div className="container w-auto bg-black bg-opacity-0 gap-3 px-5 rounded-2xl flex self-center group-hover:bg-opacity-80 transition-bg-opacity ease-in-out duration-300 lg:hidden">
       {list.map((item) => (
-        <div key={item.id} className="">
+        <div key={item.id} className="w-fit">
           <a href={item.href} className='opacity-50 hover:opacity-80'>
             <i className={`${item.icon} text-xl`}></i>
           </a>
