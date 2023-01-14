baef80a0f1009b82280b4573cb587de0185ce84a
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-14 19:37:31 -0300

    Commit 74: Removed idioms from Resume

--- src/components/main-view/resume/Resume.tsx
+++ src/components/main-view/resume/Resume.tsx
@@ -1,12 +1,10 @@
 // Components
 import Education from './education/Education';
-import Idioms from './idioms/Idioms';
 import Skills from './skills/Skills';
 
 // Const
 import { educationList } from './education/education-list/education-list';
 import { educationImgList } from './education/education-img-board/education-img-list/education-img-list';
-import { idiomList } from './idioms/idiom-list/idiom-list';
 import { skillList } from './skills/skill-list/skill-list';
 
 // Resume main function
@@ -18,7 +16,7 @@ export default function Resume() {
     >
       <Education list={educationList} imgList={educationImgList}></Education>
       <Skills list={skillList}></Skills>
-      <Idioms list={idiomList}></Idioms>
+
       <div className="rounded-xl bg-red-900 px-4 sm:px-7 md:px-8 lg:px-10"></div>
     </div>
   );
