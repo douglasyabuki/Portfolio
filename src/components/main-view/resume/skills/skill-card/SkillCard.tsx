cc34e83f7551a5d397d1533b00e98012ba3907be
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-14 14:28:34 -0300

    Commit 71: Bootstrap new comps on skills section

--- src/components/main-view/resume/skills/skill-card/SkillCard.tsx
+++ src/components/main-view/resume/skills/skill-card/SkillCard.tsx
@@ -1,18 +1,35 @@
 // Interfaces
-import { ISkill } from "../../../../../interfaces/ISkill";
+import { ISkill } from '../../../../../interfaces/ISkill';
 
 // Props destructuring
 interface Props {
-  item: ISkill
+  item: ISkill;
 }
 
 // Skill Card (Tech stack) main function
-export default function SkillCard ({item}: Props) {
+export default function SkillCard({ item }: Props) {
 
   // Returns a single card to Skills.tsx
   return (
-    <div>
-
+    <div className="group container flex h-20 w-20 scale-100 items-center justify-center rounded-xl bg-background-div1 shadow-md shadow-black transition-transform duration-300 hover:scale-110 hover:shadow-lg hover:shadow-black xs:h-24 xs:w-24 md:h-40 md:w-40">
+      <div className="block">
+        <a className="h-auto w-auto" href={item.skillUrl}>
+          {item.skillImg ? (
+            <img
+              src={item.skillImg}
+              alt={item.skillImgAlt}
+              className="cover m-1 h-5 w-5 opacity-60 transition-opacity duration-300 group-hover:opacity-100 xs:h-6 xs:w-6 md:h-9 md:w-9"
+            ></img>
+          ) : (
+            <i
+              className={`${item.skillIcon} text-xl opacity-60 transition-opacity duration-300 group-hover:opacity-100 xs:text-2xl md:text-4xl`}
+            ></i>
+          )}
+          <h2 className="-mt-3 text-xs opacity-0 transition-all duration-300 ease-in-out group-hover:m-0 group-hover:opacity-80 xs:text-sm md:text-lg">
+            {item.skillName}
+          </h2>
+        </a>
+      </div>
     </div>
   );
 }
---
+++ src/components/main-view/resume/skills/skill-hide-button/SkillHideButton.tsx
@@ -0,0 +1,39 @@
+// Props destructuring
+interface Props {
+  isActive: boolean;
+  setIsActive: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+// SkillHideButton main function
+export default function SkillHideButton({ isActive, setIsActive }: Props) {
+
+  // Function that deactivates parent state when clicking on eye icon
+  const pokeEye = () => {
+    setIsActive(false);
+  };
+
+  // Function that activates parent state when clicking on eyedropper icon
+  const useEyeDropper = () => {
+    setIsActive(true);
+  };
+
+  // Returns interactive button to Skills.tsx
+  return (
+    <div className="relative m-auto flex h-12 w-12 items-center justify-center rounded-xl bg-background-div1 shadow-md shadow-black transition-all duration-300 hover:shadow-lg hover:shadow-black md:h-20 md:w-20">
+      <i
+        role={'button'}
+        onClick={useEyeDropper}
+        className={`${
+          isActive ? 'h-0 w-0 opacity-0' : 'animate-bounce opacity-100'
+        } fa fa-eyedropper absolute -top-5 -right-2 h-auto w-auto text-2xl hover:scale-110 md:-top-10 md:-right-3 md:text-5xl`}
+      ></i>
+      <i
+        role={'button'}
+        onClick={pokeEye}
+        className={`${
+          isActive ? 'fa fa-eye' : 'fa fa-eye-slash'
+        } h-fit text-2xl transition-all duration-300 hover:scale-110 md:text-5xl`}
+      ></i>
+    </div>
+  );
+}
