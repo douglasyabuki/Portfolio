0791eb869e79825633748ccc427b10b008fed97f
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-14 08:45:47 -0300

    Commit 68: Bootstrap Skills (tech stack) component

--- src/components/main-view/resume/skills/Skills.tsx
+++ src/components/main-view/resume/skills/Skills.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function Skills ({list}: Props) {
   return (
-    <div className="container px-4 sm:px-7 md:px-8 lg:px-10 py-20">
+    <div className="w-auto space-y-12 px-4 py-20 sm:px-7 md:px-8 lg:px-10 text-center">
       <h1 className="w-auto text-3xl font-bold md:text-4xl xl:text-5xl 2xl:text-6xl">Tech Stack</h1>
     </div>
   );
---
+++ src/components/main-view/resume/skills/skill-card/SkillCard.tsx
@@ -0,0 +1,18 @@
+// Interfaces
+import { ISkill } from "../../../../../interfaces/ISkill";
+
+// Props destructuring
+interface Props {
+  item: ISkill
+}
+
+// Skill Card (Tech stack) main function
+export default function SkillCard ({item}: Props) {
+
+  // Returns a single card to Skills.tsx
+  return (
+    <div>
+
+    </div>
+  );
+}
