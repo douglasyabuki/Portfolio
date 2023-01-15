4a64de3322a3bad2077c4393c2bbd4dbc3994133
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-15 16:01:00 -0300

    Commit 95: Fixed shadow not working

--- src/components/main-view/resume/downloadable-resume/resume-chooser/pdf-hole/PdfHole.tsx
+++ src/components/main-view/resume/downloadable-resume/resume-chooser/pdf-hole/PdfHole.tsx
@@ -8,9 +8,7 @@ interface Props {
 export default function PdfHole({ item }: Props) {
   return (
     <div
-      className={`flex h-32 w-32 items-center justify-center rounded-xl shadow-inner${
-        item.id + 1
-      } transition-all duration-300 hover:translate-y-1 hover:shadow-inner${item.id + 1}-hover`}
+      className={`flex h-32 w-32 items-center justify-center rounded-xl shadow-inner${item.id+1}  transition-all duration-300 hover:translate-y-1 hover:shadow-inner${item.id+1}-hover `}
     >
       <a href={item.downloadPath}>
         <img
