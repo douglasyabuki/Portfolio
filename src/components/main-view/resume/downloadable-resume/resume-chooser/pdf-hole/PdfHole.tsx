9230eec3715bfe8e687559e7e0cb77cc72e2c0fe
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-16 07:18:59 -0300

    Commit 96: Minor changes

--- src/components/main-view/resume/downloadable-resume/resume-chooser/pdf-hole/PdfHole.tsx
+++ src/components/main-view/resume/downloadable-resume/resume-chooser/pdf-hole/PdfHole.tsx
@@ -11,9 +11,7 @@ export default function PdfHole({ item }: Props) {
   // Exports a hole-shaped div with a pdf anchor inside it to ResumeChooser.tsx
   return (
     <div
-      className={`flex h-32 w-32 items-center justify-center rounded-xl shadow-inner${
-        item.id + 1
-      }  transition-all duration-300 hover:translate-y-1 hover:shadow-inner${item.id + 1}-hover `}
+      className={`flex h-32 w-32 items-center justify-center rounded-xl ${`shadow-inner${item.id+1}`}  transition-all duration-300 hover:translate-y-1 ${`hover:shadow-inner${item.id+1}-hover`}`}
     >
       <a href={item.downloadPath} download>
         <img
