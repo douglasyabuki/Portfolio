c816706f257b0b5f36902f662b0e688cf03da36a
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-16 08:07:47 -0300

    Commit 99: Message on projects to visitors

--- src/components/main-view/projects/Projects.tsx
+++ src/components/main-view/projects/Projects.tsx
@@ -8,7 +8,10 @@ export default function Projects(props: IProjectsProps) {
       id="#projects"
       className="h-auto w-auto bg-background-div2 py-16 px-4 sm:px-7 md:px-8 lg:px-10"
     >
-      <div className="rounded-xl bg-red-700 px-4 sm:px-7 md:px-8 lg:px-10">Project works</div>
+      <div className="rounded-xl bg-red-700 px-4 sm:px-7 md:px-8 lg:px-10">
+        <h1 className='text-xl'>Projects section under construction!</h1>
+        <h1 className='text-lg'>Meanwhile, please check my <a href='https://github.com/douglasyabuki' className='underline'>{` github page`}</a></h1>
+      </div>
     </div>
   );
 }
