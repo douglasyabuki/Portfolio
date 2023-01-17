6390cb1c59c42d367e8d72a04f22fd4933e8f18d
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-17 15:35:05 -0300

    Commit 123: Fewer changes to test cards layout

--- src/components/main-view/projects/project-cards/ProjectCards.tsx
+++ src/components/main-view/projects/project-cards/ProjectCards.tsx
@@ -11,14 +11,13 @@ export default function ProjectCards({ list }: Props) {
   return (
     <div className="w-auto space-y-12 px-4 py-20 sm:px-7 md:px-8 lg:px-10">
       <div className='block w-full justify-center py-8 text-center lg:flex lg:justify-between lg:text-left'>
-        <h1 className="w-auto text-center text-3xl font-bold md:text-4xl lg:text-left xl:text-5xl 2xl:text-6xl">Projects</h1>
-        <h2 className="py-4 text-xl opacity-70 md:text-2xl lg:w-2/5 lg:py-0 2xl:text-4xl">
+        <h2 className="w-auto text-center text-3xl font-bold md:text-4xl lg:text-left xl:text-5xl 2xl:text-6xl">Projects</h2>
+        <h3 className="py-4 text-xl opacity-70 md:text-2xl lg:w-2/5 lg:py-0 2xl:text-4xl">
           Take a quick look at some of his projects. Github and website available to each one of
           these.
-        </h2>
+        </h3>
       </div>
-
-      <div>
+      <div className='flex-col space-y-20 items-center justify-center'>
         {list.map((item) => (
           <ProjectCard key={item.id} item={item}></ProjectCard>
         ))}
--- src/components/main-view/projects/project-cards/project-card/ProjectCard.tsx
+++ src/components/main-view/projects/project-cards/project-card/ProjectCard.tsx
@@ -1,14 +1,35 @@
 // Interfaces
-import { IProject } from "../../../../../interfaces/IProject";
+import { IProject } from '../../../../../interfaces/IProject';
 
+// Hooks
+import { useState } from 'react';
+
+// Props destructuring
 interface Props {
   item: IProject;
 }
 
-export default function ProjectCard ({item}: Props) {
-  return (
-    <div>
+// Project Card main function
+export default function ProjectCard({ item }: Props) {
+  const [isExtended, setIsExtended] = useState<boolean>(false);
 
+  item.id;
+  item.name;
+  item.description;
+  item.url;
+  item.gitUrl;
+  item.techs;
+
+  // Returns a single card to be rendered by ProjectCards.tsx
+  return (
+    <div className="container relative m-auto h-auto w-fit flex-col items-center justify-between bg-background-div1 rounded-xl">
+      <h1 className='mb-2 text-center text-xl font-bold md:text-2xl xl:mb-4 xl:text-3xl 2xl:text-4xl'>{item.name}</h1>
+      <div className="flex">
+        <img className="cover shaped-div1" src={item.imageSrc} alt={item.imageAlt} />
+      </div>
+        <div className="z-40 h-96 rounded-xl">
+          <h2>{item.description}</h2>
+        </div>
     </div>
   );
 }
