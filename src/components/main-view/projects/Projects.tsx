eb74ec0ba5294602d8da0942d2160a823dcfee82
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-17 08:37:36 -0300

    Commit 113: Bootstrap projects

--- src/components/main-view/projects/Projects.tsx
+++ src/components/main-view/projects/Projects.tsx
@@ -1,16 +1,15 @@
-import * as React from 'react';
-
-export interface IProjectsProps {}
-
-export default function Projects(props: IProjectsProps) {
+// Projects main function
+export default function Projects() {
   return (
     <div
       id="#projects"
-      className="h-auto w-auto bg-background-div2 py-16 px-4 sm:px-7 md:px-8 lg:px-10"
+      className="h-auto w-auto bg-background-div2 px-4 py-20 sm:px-7 md:px-8 lg:px-10"
     >
-      <div className="rounded-xl bg-red-700 px-4 sm:px-7 md:px-8 lg:px-10">
-        <h1 className='text-xl'>Projects section under construction!</h1>
-        <h1 className='text-lg'>Meanwhile, please check my <a href='https://github.com/douglasyabuki' className='underline'>{` github page`}</a></h1>
+      <div className="block w-full justify-center py-8 text-center lg:flex lg:justify-between lg:px-10 lg:text-left xl:px-20">
+        <h1 className="w-auto text-3xl font-bold md:text-4xl xl:text-5xl 2xl:text-6xl">Projects</h1>
+        <h2 className="py-4 text-xl opacity-70 md:text-2xl lg:w-2/5 lg:py-0 2xl:text-4xl">
+          Take a quick look at some of his projects. Github and website available to each one of these.
+        </h2>
       </div>
     </div>
   );
