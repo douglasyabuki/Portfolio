f0bf81b782d1b37989b73d2fdf633f581c5c9870
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-01-17 13:36:07 -0300

    Commit 121: Minor changes on Projects

--- src/components/main-view/projects/Projects.tsx
+++ src/components/main-view/projects/Projects.tsx
@@ -1,16 +1,21 @@
+// Components
+import ProjectCards from "./project-cards/ProjectCards";
+import GithubBoard from "./github-board/GithubBoard";
+
+// Const
+import { projectList } from "./project-cards/project-list/project-list";
+
 // Projects main function
 export default function Projects() {
+
+  // Returns the whole Projects section to MainView.tsx
   return (
     <div
       id="#projects"
       className="h-auto w-auto bg-background-div2 px-4 py-20 sm:px-7 md:px-8 lg:px-10"
     >
-      <div className="block w-full justify-center py-8 text-center lg:flex lg:justify-between lg:px-10 lg:text-left xl:px-20">
-        <h1 className="w-auto text-3xl font-bold md:text-4xl xl:text-5xl 2xl:text-6xl">Projects</h1>
-        <h2 className="py-4 text-xl opacity-70 md:text-2xl lg:w-2/5 lg:py-0 2xl:text-4xl">
-          Take a quick look at some of his projects. Github and website available to each one of these.
-        </h2>
-      </div>
+      <ProjectCards list={projectList}></ProjectCards>
+      <GithubBoard></GithubBoard>
     </div>
   );
 }
--- src/components/main-view/projects/project-cards/ProjectCards.tsx
+++ src/components/main-view/projects/project-cards/ProjectCards.tsx
@@ -1,14 +1,25 @@
 // Interfaces
-import { IProject } from "../../../../interfaces/IProject";
+import { IProject } from '../../../../interfaces/IProject';
 
+// Components
+import ProjectCard from './project-card/ProjectCard';
 interface Props {
   list: IProject[];
 }
 
-export default function ProjectCards ({list}: Props) {
+export default function ProjectCards({ list }: Props) {
   return (
-    <div>
-
+    <div className="block w-full justify-center py-8 text-center lg:flex lg:justify-between lg:px-10 lg:text-left xl:px-20">
+      <h1 className="w-auto text-3xl font-bold md:text-4xl xl:text-5xl 2xl:text-6xl">Projects</h1>
+      <h2 className="py-4 text-xl opacity-70 md:text-2xl lg:w-2/5 lg:py-0 2xl:text-4xl">
+        Take a quick look at some of his projects. Github and website available to each one of
+        these.
+      </h2>
+      <div>
+        {list.map((item) => (
+          <ProjectCard key={item.id} item={item}></ProjectCard>
+        ))}
+      </div>
     </div>
   );
 }
