4df24a2126cb3e39981e752455a6c2615f6d0a37
douglasyabuki <douglasyabuki@gmail.com>
2024-09-10 08:06:13 -0300

    Update: Minor layout changes

--- src/components/main-view/home/buttons/contact-button/ContactButton.tsx
+++ src/components/main-view/home/buttons/contact-button/ContactButton.tsx
@@ -1,16 +1,15 @@
 // Utils
-import { scrollOnClick } from "../../../../../utils/scroll-on-click";
+import { scrollOnClick } from '../../../../../utils/scroll-on-click';
 
 // Contact button main function
 export default function ContactButton() {
-
   // Returns a routing button to Home.tsx
   return (
     <div className="container m-5 h-auto w-auto md:m-0">
       <a
         href="#contact"
         onClick={() => scrollOnClick('#contact')}
-        className="rounded-full bg-not-so-white px-6 py-2 align-middle font-semibold text-background-black transition-all duration-300 hover:bg-background-black hover:text-not-so-white xs:text-lg md:text-xl 2xl:text-2xl 3xl:text-3xl shadow-lg shadow-translucid-black"
+        className="flex items-center rounded-full bg-not-so-white px-6 py-2 align-middle font-semibold text-background-black shadow-lg shadow-translucid-black transition-all duration-300 hover:bg-background-black hover:text-not-so-white xs:text-lg md:text-xl 2xl:text-2xl 3xl:text-3xl"
       >
         <i className="fa fa-envelope mr-2"></i>
         Contact
--- src/components/main-view/home/buttons/learn-more-button/LearnMoreButton.tsx
+++ src/components/main-view/home/buttons/learn-more-button/LearnMoreButton.tsx
@@ -1,16 +1,15 @@
 // Utils
-import { scrollOnClick } from "../../../../../utils/scroll-on-click";
+import { scrollOnClick } from '../../../../../utils/scroll-on-click';
 
 // Learn more button main function
 export default function LearnMoreButton() {
-
   // Returns a routing button to Home.tsx
   return (
-    <div className="container m-5 md:m-0 h-auto w-auto">
+    <div className="container m-5 h-auto w-auto md:m-0">
       <a
         href="#about"
         onClick={() => scrollOnClick('#about')}
-        className="rounded-full w-max bg-background-div2 px-3 py-2 align-middle font-semibold text-not-so-white transition-all  duration-300 hover:bg-not-so-white hover:text-background-black xs:text-lg md:text-xl 2xl:text-2xl 3xl:text-3xl shadow-lg shadow-translucid-black"
+        className="flex w-max items-center rounded-full bg-background-div2 px-3 py-2 align-middle font-semibold text-not-so-white shadow-lg  shadow-translucid-black transition-all duration-300 hover:bg-not-so-white hover:text-background-black xs:text-lg md:text-xl 2xl:text-2xl 3xl:text-3xl"
       >
         <i className="fa fa-info-circle mr-2"></i>
         Learn more
